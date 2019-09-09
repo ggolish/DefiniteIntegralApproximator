cefd23cba7615dc6995140092ed832c82cebf6ff
ggolish <ggolish@sycamores.indstate.edu>
2019-09-08 20:11:07 -0400

    Implemented stablization technique.

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 int main(int argc, char *argv[])
 {
@@ -22,7 +23,25 @@ int main(int argc, char *argv[])
   }
   
   ast_t *ast = parse_equ(equ);
-  printf("%.3Lf\n", integrate_simpson(ast, a, b, 4));
+
+  // TODO: Move the stablizing process below to its own function in the simpson
+  // module
+
+  // Keep approximating the integral, doubling the number of subdivisions each
+  // time, until the approximation stablizes
+  long double k = 2;
+  long double ans = integrate_simpson(ast, a, b, k);
+  for(;;) {
+    k *= 2;
+    long double curr = integrate_simpson(ast, a, b, k);
+    if(fabsl(curr - ans) < 0.0001L) {  // Ensures accurracy to 3 decimal places
+      ans = curr;
+      break;
+    }
+    ans = curr;
+  }
+
+  printf("%.3Lf\n", ans);
 
   return EXIT_SUCCESS;
 }
