6b1c255c3c6be40348eeedd7de21526ac1717fb6
ggolish <ggolish@sycamores.indstate.edu>
2019-10-06 14:56:37 -0400

    Reorganized code.

--- parser.h
+++
@@ -1,9 +0,0 @@
-#ifndef PARSER_H_
-#define PARSER_H_
-
-#include "scanner.h"
-#include "ast.h"
-
-ast_t *parse_equ(char *equ);
-
-#endif
--- simpson.h
+++
@@ -1,8 +0,0 @@
-#ifndef SIMPSON_H_
-#define SIMPSON_H_
-
-#include "ast.h"
-
-long double integrate_simpson(ast_t *ast, long double a, long double b, int n);
-
-#endif
